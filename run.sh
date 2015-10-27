e7a5041f36ca4946cc5d18a77711ee91e369f408
Elliott Clark <elliott@fb.com>
2015-10-26 23:37:13 -0700

    More iterations

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ for FT in csv json
 do
   OFILE="result.${FT}"
   [[ -f "$OFILE" ]] &&  rm ${OFILE}
-  java ${JAVA_ARGS} -jar target/benchmarks.jar -rf ${FT} -rff ${OFILE} "$@"
+  java ${JAVA_ARGS} -jar target/benchmarks.jar -rf ${FT} -rff ${OFILE} -i 20 -to 20min "$@"
 done
